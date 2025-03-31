abd58eef5646d1c22877a19da92de464d798dc60
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-03-31 11:34:06 +0700

    Analytics Page Layout

--- src/views/AnalyticsPage.vue
+++ src/views/AnalyticsPage.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>Analytics</div>
+  <div class="analytics-page">
+    <div class="graphs-section">
+      <ContainerCard> </ContainerCard>
+
+      <ContainerCard> </ContainerCard>
+
+      <ContainerCard> </ContainerCard>
+
+      <ContainerCard> </ContainerCard>
+    </div>
+  </div>
 </template>
+
+<script>
+import ContainerCard from "@/components/UI/ContainerCard.vue";
+
+export default {
+  components: {
+    ContainerCard,
+  },
+};
+</script>
+
+<style scoped>
+.graphs-section {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+  row-gap: 20px;
+}
+</style>
